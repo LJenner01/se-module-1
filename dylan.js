63e37d74fc57a74b3376c007a9098af858c7eec3
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-07 14:08:06 +0800

    Q3 completed

--- dylan.js
+++ dylan.js
@@ -1,6 +1,19 @@
+var Sentiment = require('sentiment');
 
 function dylanFunction(){
     console.log("Dylans Function");
+    var sentiment = new Sentiment();
+var result = sentiment.analyze('Cats are stupid.');
+console.dir(result);    // Score: -2, Comparative: -0.666
     };
 
-    module.exports = { dylanFunction };
+module.exports = dylanFunction;
+
+
+// 3. Write a function to convert Celsius to Fahrenheit
+
+function calFahrenheit(temp) {
+console.log(temp * (9/5) + 32);
+};
+calFahrenheit(34);
+
