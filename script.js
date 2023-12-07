5edacdba54d6c494c5c5f9b5cf27cb9c7f4f97c7
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-07 10:46:08 +0800

    dylan js file

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-import { JamesB } from './james.js';
+
 
 function main(){
     console.log('getting started...');
     dylanFunction();
     JamesB();
+    ernieWork();
+    lukeWork();
 };
 
 function dylanFunction(){
 console.log("Dylans Function");
 };
 
-<<<<<<< HEAD
-function JamesB() {
-    console.log("This function was made by James")};
-=======
+
 function lukeWork(){
     console.log('lukes function');
-}
+};
 
 function ernieWork(){
     console.log('ernie function')
-}
->>>>>>> d30b2459b4a19100822ff93cffe46465be25f0c6
+};
+
