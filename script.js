c13b7f27e56d325bfaccb034e3dd6a73e5d8f387
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-07 10:41:59 +0800

    dyl james function add

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function main(){
     console.log('getting started...');
-    JamesB()
-}
+    dylanFunction();
+    JamesB();
+};
+
+function dylanFunction(){
+console.log("Dylans Function");
+};
+
 function JamesB() {
-    console.log("This function was made by James")
-}
+    console.log("This function was made by James")};
