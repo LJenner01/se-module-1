7174873dc06c2ac6ab586ff75a1de00eeb830b3b
AlexanderSeyler <alexanderseyler@gmail.com>
2023-12-07 14:35:41 +0800

    exercise 4 alex

--- alex.js
+++ alex.js
@@ -3,14 +3,19 @@ function Alex(){
     display();
     let answer = findSquare(4);
     let celsius = Fahrenheit(20);
+    let Area = measurements(5,6);
     console.log(answer);
     console.log(celsius);
+    console.log(Area);
 }
 function display() {
     console.log("Hello from Alex");
 }
 function Fahrenheit(cel) {
     return (cel*9/5)+32;
+}
+function measurements(a,b){
+    return (a*b);
 }
 
 function findSquare(a){
