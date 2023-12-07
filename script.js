d408b58451291d8401260c6056abd2fff7ebc470
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-07 10:37:41 +0800

    commit for james and dylans function add

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 function main(){
     console.log('getting started...');
-<<<<<<< HEAD
     dylanFunction();
-}
+    JamesB();
+};
 
 function dylanFunction(){
-
 console.log("Dylans Function");
-=======
-    JamesB()
-}
+};
+
 function JamesB() {
-    console.log("This function was made by James")
->>>>>>> 46883c49425ccfada93a89d47ed55bbc02c272ad
-}
+    console.log("This function was made by James")};
