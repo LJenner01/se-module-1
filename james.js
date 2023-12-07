5bb9a0af4fb711e02b3febc90d71252ead6dfa95
JamesBain999 <baino999@gmail.com>
2023-12-07 14:08:21 +0800

    Exercise 3 James

--- james.js
+++ james.js
@@ -1,5 +1,20 @@
 function JamesB() {
-    console.log("This function was made by James")    
+    console.log("This function was made by James");
+    displayMessage();
+    findSquare(8);
+    calFahrenheit(100);
+}
+
+function displayMessage() {
+    console.log("Hello Boys!!")
+}
+
+function findSquare(a) {
+    return console.log(a*a)
+}
+
+function calFahrenheit(cel) {
+    return console.log((cel * 9/5) + 32);
 }
 
 module.exports = JamesB;
