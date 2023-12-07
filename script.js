05f5fdaba2931b0dd36c170e8e236beeca30514a
LJenner01 <luke.jenner@institutedata.com>
2023-12-07 13:13:24 +1030

    lukes external js

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ function JamesB() {
     console.log("This function was made by James")    
 }
 
-function lukeWork(){
-    console.log('lukes function');
-}
 
 function ernieWork(){
     console.log('ernie function')
