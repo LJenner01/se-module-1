bc96fd7293b4bbc8c28f66a4059974a0f4f14ec6
Keestle <yeeern98@gmail.com>
2023-12-07 14:35:00 +0800

    semi-colons added

--- ernie.js
+++ ernie.js
@@ -2,17 +2,22 @@ function ernieWork(){
     const Sentiment = require('sentiment');
     const sentiment = new Sentiment();
     const result = sentiment.analyze('cats are stupid');
-    console.log('result')
-    console.log(calFahrenheit(32))
-    console.log(rectangleArea(10,50))
+    console.log('result');
+    console.log(calFahrenheit(32));
+    console.log(rectangleArea(10,50));
+    // console.log(radius(10));
 }
 
 module.exports = ernieWork;
 
 function calFahrenheit(cel){
-return (cel*9/5)+32
+return (cel*9/5)+32;
 }
 
 function rectangleArea(a,b){
     return `Area rectangle ${a * b}`;
 }
+
+// function circleValues(radius){
+//     return 
+// }
