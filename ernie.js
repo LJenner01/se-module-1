6e8219a5eb4697834c8f7f5aebbeea8a0359e792
Keestle <yeeern98@gmail.com>
2023-12-07 14:29:18 +0800

    Question 4 added

--- ernie.js
+++ ernie.js
@@ -4,7 +4,7 @@ function ernieWork(){
     const result = sentiment.analyze('cats are stupid');
     console.log('result')
     console.log(calFahrenheit(32))
-    console.log(area)
+    console.log(rectangleArea(10,50))
 }
 
 module.exports = ernieWork;
@@ -13,8 +13,6 @@ function calFahrenheit(cel){
 return (cel*9/5)+32
 }
 
-function rectangleArea(area){
-    return
-
-
+function rectangleArea(a,b){
+    return `Area rectangle ${a * b}`;
 }
