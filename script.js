f18507aa4bb96c7fbc65a16174cff2ec6ae20655
AlexanderSeyler <alexanderseyler@gmail.com>
2023-12-07 10:49:46 +0800

    changes by alex

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ function dylanFunction(){
 console.log("Dylans Function");
 };
 
-<<<<<<< HEAD
-function JamesB() {
-    console.log("This function was made by James")};
-=======
 function lukeWork(){
     console.log('lukes function');
 }
@@ -21,4 +17,4 @@ function lukeWork(){
 function ernieWork(){
     console.log('ernie function')
 }
->>>>>>> d30b2459b4a19100822ff93cffe46465be25f0c6
+include("alex.js");
