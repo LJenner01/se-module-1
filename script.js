da07ee860d93b463a6389b7bb2a9d7edf22d43cf
LJenner01 <luke.jenner@institutedata.com>
2023-12-08 11:12:23 +1030

    Quick tidy up, removing encryption package and test code

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const dylanFunction = require("./dylan.js");
 const Alex = require("./alex.js");
 const ernieWork = require("./ernie.js");
 const lukeWork = require("./lukes.js");
-const { encrypt, decrypt } = require('encryption-toolkit');
-
 
 function main(){
     console.log('getting started...');
@@ -12,16 +10,6 @@ function main(){
     lukeWork();
     ernieWork();
     Alex();
-    dylanFunction();
- 
-    const secretKey = 'my_secret_key'; const plainText = 'Hello, World!';
-
-    const encryptedData = encrypt(plainText, secretKey); 
-    console.log('Encrypted:', encryptedData);
-    
-    const decryptedText = decrypt(encryptedData, secretKey); 
-    console.log('Decrypted:', decryptedText);
-
-    console.log(decryptedText);
+    dylanFunction(); 
 }
-console.log(main())
+main();
