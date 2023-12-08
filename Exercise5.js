514fdc6548cb435e67b7d281dce8a34dd09cfd6b
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-08 14:20:22 +0800

    exercise5

---
+++ Exercise5.js
@@ -0,0 +1,18 @@
+//For understanding more about arrays, try creating an array that has 5 elements.
+let array = [1, 2, 3, 4, 5]
+
+//Replace the value of the element at position 1 and 4.
+//Add a new element to the beginning of the array
+//Remove the element at the end of the array
+//Print the array to the console.
+
+array.shift();
+array.unshift(22);
+console.log(array);
+
+array.pop();
+array.push(44);
+console.log(array);
+
+
+
